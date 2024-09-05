0cabb9a3a6817161841e135a346ee15b5c0ab024
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-05 08:42:11 +0300

    This is first commit with the API call working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
+class  App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       user:[]
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/users')//url to our API
+    .then(response => response.json())
+    .then(res => {
+      if (res.data){
+        console.log(res.data)
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
+}
 }
 
 export default App;
